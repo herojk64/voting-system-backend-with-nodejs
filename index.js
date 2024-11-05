75e6381093553295c7d9d7773e6f8e35894f0dca
Amit Dhakal <herojk64@gmail.com>
2024-11-05 17:43:56 +0545

    Testing hosting server response and all

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ require('dotenv').config();
 app.use(express.json())
 app.use(cors());
 
+app.get("/",function(req,res){
+  return res.status(200).json({
+    message:"Success"
+  })
+});
+
 app.listen(port, () => {
   console.log(`Port is listening at http://localhost:${port}`)
 })
